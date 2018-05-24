39bb26813df65ef29d527a7a95eeb094e2c92577
utkuufuk <utkuufuk@gmail.com>
2018-05-24 19:01:57 +0300

    reverse the dates on the bar chart

--- scraper.py
+++ scraper.py
@@ -11,6 +11,7 @@ AGENT = "Mozilla/5.0 Chrome/47.0.2526.106 Safari/537.36"
 SITE_URL = "https://tr.investing.com/currencies/"
 USD_SUFFIX = "usd-try-historical-data"
 EUR_SUFFIX = "eur-try-historical-data"
+BAR_WIDTH = 0.25
 
 def createSoup(url):
     return BeautifulSoup(requests.get(url, headers={"User-Agent": AGENT}).text, "lxml")
@@ -26,26 +27,32 @@ def getRateHistory(url, rates):
         rate = columns[1].getText()
         rates[date] = float(rate.replace(',', '.'))
 
-if __name__ == '__main__':
-    usdRates = OrderedDict()
-    getRateHistory(SITE_URL + USD_SUFFIX, usdRates)
-    with open("usd.json", 'w') as fp:
-        json.dump(usdRates, fp)
+# fetch USD history
+usdRates = OrderedDict()
+getRateHistory(SITE_URL + USD_SUFFIX, usdRates)
+with open("usd.json", 'w') as fp:
+    json.dump(usdRates, fp)
 
+# fetch EURO history
+eurRates = OrderedDict()
+getRateHistory(SITE_URL + EUR_SUFFIX, eurRates)
+with open("euro.json", 'w') as fp:
+    json.dump(eurRates, fp)
 
-    eurRates = OrderedDict()
-    getRateHistory(SITE_URL + EUR_SUFFIX, eurRates)
-    with open("euro.json", 'w') as fp:
-        json.dump(eurRates, fp)
+# reverse the date order
+usdRates = OrderedDict(reversed(usdRates.items()))
+eurRates = OrderedDict(reversed(eurRates.items()))
 
-    numBins = len(list(usdRates.keys()))
-    index = np.arange(numBins)
-    fig, ax = plt.subplots()
-    ax.bar(index, list(usdRates.values()), 0.4)
-    ax.bar(index + 0.4, list(eurRates.values()), 0.4)
-    ax.set_xticklabels(list(usdRates.keys()))
-    ax.set_xticks(index + 0.4 / 2)
-    ax.legend(['USD', 'EURO'])
-    ax.grid()
-    fig.tight_layout()
-    plt.show()
+# plot the historical data
+numBins = len(list(usdRates.keys()))
+xIndices = np.arange(numBins)
+fig, ax = plt.subplots()
+ax.bar(xIndices, list(usdRates.values()), BAR_WIDTH, label="USD")
+ax.bar(xIndices + BAR_WIDTH, list(eurRates.values()), BAR_WIDTH, label="EURO")
+ax.set_xticklabels([label[:-5] for label in list(usdRates.keys())])
+ax.set_xticks(xIndices + BAR_WIDTH / 2)
+ax.legend()
+ax.grid()
+fig.tight_layout()
+plt.axis([0, numBins, 3, 6])
+plt.show()
